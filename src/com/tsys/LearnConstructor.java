cb4e699b99df6b8c17e7d076445066f15ec7e533
bindut94 <37606799+bindut94@users.noreply.github.com>
2021-07-26 20:26:47 +0100

    Update LearnConstructor.java

--- src/com/tsys/LearnConstructor.java
+++ src/com/tsys/LearnConstructor.java
@@ -3,12 +3,16 @@ package com.tsys;
 public class LearnConstructor {
 	
 	String xyz ;
+	String color;
+	int size;
 	
-	public LearnConstructor(String input) {
+	public LearnConstructor(String input, String color, int size) {
 		   
 	    this.xyz = input;
+	    this.color = green;
+	    this.size = 5;
 		
-		System.out.println("Hey I have parameterized constructor, Default is gone..Yay!!!" + input);	
+		System.out.println("Hey I have parameterized constructor, Default is gone..Yay!!!" + input, " ", + color, " ", + size);	
 		
 		}
 
@@ -18,6 +22,7 @@ public class LearnConstructor {
 	public static void main(String[] args) {	
 		
 		LearnConstructor secondConstructor = new LearnConstructor("Bindhu");
+	        
 		
 		System.out.println(secondConstructor.xyz);
 		System.out.println(secondConstructor.xyz);
